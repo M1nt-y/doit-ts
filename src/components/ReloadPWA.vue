a2d4f73322c75730fec03c260034d35afb012800
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-04-04 21:32:35 +0300

    Configure PWA

--- src/components/ReloadPWA.vue
+++ src/components/ReloadPWA.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
 import { useRegisterSW } from 'virtual:pwa-register/vue';
 
-const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
+// replaced dyanmicaly
+const reloadSW: any = '__RELOAD_SW__'
+const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
+  immediate: true,
+  onRegisteredSW(swUrl, r) {
+    // eslint-disable-next-line no-console
+    console.log(`Service Worker at: ${swUrl}`)
+    if (reloadSW === 'true') {
+      r && setInterval(async () => {
+        // eslint-disable-next-line no-console
+        console.log('Checking for sw update')
+        await r.update()
+      }, 20000 /* 20s for testing purposes */)
+    }
+    else {
+      // eslint-disable-next-line no-console
+      console.log(`SW Registered: ${r}`)
+    }
+  },
+})
 const close = async () => {
   offlineReady.value = false;
   needRefresh.value = false;
@@ -13,24 +32,42 @@ async function handleUpdateServiceWorker() {
 </script>
 
 <template>
-  <!--  need styling -->
-  <div v-if="offlineReady || needRefresh" class="flex flex-wrap" role="alert">
-    <div class="message mt-1">
+  <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
+    <div class="message">
       <span v-if="offlineReady"> App ready to work offline </span>
       <span v-else>New content available, click on reload button to update.</span>
     </div>
-    <div class="buttons flex align-middle mt-2 md:mt-0">
-      <button v-if="needRefresh" @click="handleUpdateServiceWorker" class="button">
-        Reload
-      </button>
-      <button @click="close" class="button">
-        Close
-      </button>
-    </div>
+    <button v-if="needRefresh" @click="handleUpdateServiceWorker" class="button">
+      Reload
+    </button>
+    <button @click="close" class="button">
+      Close
+    </button>
   </div>
 </template>
 
 
 <style scoped lang="scss">
-
+.pwa-toast {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin: 16px;
+  padding: 12px;
+  border: 1px solid #8885;
+  border-radius: 4px;
+  z-index: 1;
+  text-align: left;
+  box-shadow: 3px 4px 5px 0 #8885;
+  & .message {
+    margin-bottom: 8px;
+  }
+  & button {
+    border: 1px solid #8885;
+    outline: none;
+    margin-right: 5px;
+    border-radius: 2px;
+    padding: 3px 10px;
+  }
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,44 +2,77 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
 import { VitePWA } from 'vite-plugin-pwa'
+import replace from '@rollup/plugin-replace'
 
-// https://vitejs.dev/config/
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Doit',
+    short_name: 'Doit',
+    theme_color: '#0B1729',
+    icons: [
+      {
+        src: 'android-chrome-192x192.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'android-chrome-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest';
+}
+
+if (claims)
+  pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error overrides
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+  pwaOptions.selfDestroying = selfDestroying
+
 export default defineConfig({
+  // base: process.env.BASE_URL || 'https://github.com/',
+  build: {
+    sourcemap: process.env.SOURCE_MAP === 'true',
+  },
   plugins: [
     vue(),
-    VitePWA({
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Doit',
-        short_name: 'Doit',
-        description: 'Doit description',
-        theme_color: '#ffffff',
-        start_url: '/',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: "any maskable"
-          }
-        ]
-      }
-    }),
+    VitePWA(pwaOptions),
+    replace(replaceOptions),
   ],
   server: {
     port: 3000
