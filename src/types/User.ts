18c331891d83db7756df70ad4cac2719de7d421f
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-04-11 03:59:07 +0300

    Type of current user object

---
+++ src/types/User.ts
@@ -0,0 +1,14 @@
+interface User {
+    birthdate: string,
+    blocked: boolean,
+    confirmed: boolean,
+    country: string,
+    createdAt: string,
+    email: string,
+    id: number,
+    provider: string,
+    updatedAt: string,
+    username: string
+}
+
+export default User
