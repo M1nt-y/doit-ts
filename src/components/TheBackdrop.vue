57d4d0d70c55eb4f4a227b5f72bd85471e2ed353
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-04-11 03:56:37 +0300

    Small transitions in default layout

---
+++ src/components/TheBackdrop.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useMainStore } from '@/stores/main'
+
+const mainStore = useMainStore();
+</script>
+
+<template>
+  <div class="backdrop" @click="mainStore.closeAll" />
+</template>
+
+<style scoped>
+
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import FixedNavigation from '@/components/FixedNavigation.vue'
+import TheHeader from '@/components/TheHeader.vue'
+import TheBackdrop from '@/components/TheBackdrop.vue'
+import PopUp from '@/components/PopUp.vue'
+import {useMainStore} from '@/stores/main'
 
-import FixedNavigation from "@/components/FixedNavigation.vue";
-import TheHeader from "@/components/TheHeader.vue";
+
+const mainStore = useMainStore()
 </script>
 
 <template>
-  <FixedNavigation />
   <TheHeader />
+  <FixedNavigation />
+  <transition name="fade">
+    <TheBackdrop v-if="mainStore.showBackdrop" />
+  </transition>
+  <transition name="fade">
+    <PopUp v-if="mainStore.showPopup" />
+  </transition>
   <main class="main">
     <slot />
   </main>
 </template>
 
 <style scoped lang="scss">
-
+.fade-enter-from { opacity: 0; }
+.fade-enter-to { opacity: 1; }
+.fade-enter-active { transition: opacity .6s }
+.fade-leave-from { opacity: 1; }
+.fade-leave-to { opacity: 0; }
+.fade-leave-active { transition: opacity .6s }
 </style>
