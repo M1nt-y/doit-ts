4485a825b8dbad00327c061670d9d044bd916246
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-04-11 03:59:32 +0300

    Added custom select component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  options: {
+    type: Array,
+    default: () => []
+  }
+})
+
+const isActive = ref(false)
+
+function toggleSelect() {
+  isActive.value = !isActive.value
+}
+</script>
+
+<template>
+  <div ref="selectWrapper" class="select-wrapper">
+
+    <input
+        class="input" type="text"
+        ref="customSelect" :value="modelValue"
+        @click="toggleSelect"
+        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind="$attrs"
+    />
+
+    <ul ref="customOptions" class="options" v-if="isActive">
+      <li class="option" v-for="(option, index) in options" :key="index">{{ option.name }}</li>
+    </ul>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.options {
+  margin-top: 4px;
+  max-height: 240px;
+  overflow-y: scroll;
+  border-radius: 2px;
+  border: 1px solid #1472FF;
+}
+.option {
+  padding: 10px 15px;
+  margin-right: 15px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 100%;
+  color: #627CA3;
+  cursor: pointer;
+  transition: all .3s;
+  &:hover {
+    color: #E6E6E6;
+    background: #1472FF;
+  }
+}
+</style>
